70118e5023e709583fcc87f2152545675273b5b3
Tornike Khatiashvili <95305738+Tornike1123@users.noreply.github.com>
2024-01-14 18:40:41 +0400

    migration

--- BulkyBookWeb/BulkyBookWeb/obj/Debug/net6.0/BulkyBookWeb.AssemblyInfo.cs
+++ BulkyBookWeb/BulkyBookWeb/obj/Debug/net6.0/BulkyBookWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BulkyBookWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed9ebdcb0bb85fa1103c606fff682b41ad4819ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("BulkyBookWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BulkyBookWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
