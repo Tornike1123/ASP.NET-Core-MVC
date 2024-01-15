3305738b48e4b7aa203051fc2489d920f78fae20
Tornike Khatiashvili <95305738+Tornike1123@users.noreply.github.com>
2024-01-15 14:02:37 +0400

    Retrieve all categories from database

---
+++ BulkyBookWeb/BulkyBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BulkyBookWeb.Data;
+using Microsoft.AspNetCore.Mvc;
+
+
+
+namespace BulkyBookWeb.Controllers;
+
+public class CategoryController : Controller
+{
+    private readonly ApplicationDbContext _db;
+
+    public CategoryController(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+
+    public IActionResult Index()
+    {
+        var objCategoryList = _db.Categories.ToList();
+        return View();
+    }
+}
+
--- BulkyBookWeb/BulkyBookWeb/Data/ApplicationDbContext.cs
+++ BulkyBookWeb/BulkyBookWeb/Data/ApplicationDbContext.cs
@@ -7,6 +7,7 @@ namespace BulkyBookWeb.Data;
 public class ApplicationDbContext :DbContext
 {
 
+
 	public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
 	{
 
