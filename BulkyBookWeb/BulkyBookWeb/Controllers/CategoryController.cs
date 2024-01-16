5c9cfa7a603d666af32b868721f8a8a80753cfa9
Tornike Khatiashvili <95305738+Tornike1123@users.noreply.github.com>
2024-01-16 23:05:54 +0400

    Edit Category

--- BulkyBookWeb/BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/BulkyBookWeb/Controllers/CategoryController.cs
@@ -33,7 +33,7 @@ public class CategoryController : Controller
         return View();
     }
 
-    //Post
+    //Post 
     [HttpPost]
     [ValidateAntiForgeryToken]
     public IActionResult Create(Category obj)
@@ -47,6 +47,42 @@ public class CategoryController : Controller
         _db.Categories.Add(obj);
         _db.SaveChanges();
         return RedirectToAction("Index");
+        }
+        return View(obj);
+    }
+
+    //Get
+    public IActionResult Edit(int? id)
+    {
+        if(id==null || id == 0)
+        {
+            return NotFound();
+        }
+        var categoryFromDb = _db.Categories.Find(id);
+        //var categoryFromDbFirst = _db.Categories.FirstOrDefault(u => u.Id == id);
+        //var categoryFromDbSingle = _db.Categories.SingleOrDefault(u => u.Id == id);
+
+        if (categoryFromDb == null)
+        {
+            return NotFound();
+        }
+        return View(categoryFromDb);
+    }
+
+    //Post
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Edit(Category obj)
+    {
+        if (obj.Name == obj.DisplayOrder.ToString())
+        {
+            ModelState.AddModelError("name", "The Displayorder cannot exactly match the Name.");
+        }
+        if (ModelState.IsValid)
+        {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
         }
         return View(obj);
     }
