773f09d6847d42226aec2bf55832089f410fc61a
Tornike Khatiashvili <95305738+Tornike1123@users.noreply.github.com>
2024-01-17 22:03:57 +0400

    TempData

--- BulkyBookWeb/BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/BulkyBookWeb/Controllers/CategoryController.cs
@@ -46,6 +46,7 @@ public class CategoryController : Controller
         {
         _db.Categories.Add(obj);
         _db.SaveChanges();
+            TempData["success"] = "Category created successfully";
         return RedirectToAction("Index");
         }
         return View(obj);
@@ -82,6 +83,8 @@ public class CategoryController : Controller
         {
             _db.Categories.Update(obj);
             _db.SaveChanges();
+            TempData["success"] = "Category updated successfully";
+
             return RedirectToAction("Index");
         }
         return View(obj);
@@ -119,6 +122,8 @@ public class CategoryController : Controller
 
         _db.Categories.Remove(obj);
         _db.SaveChanges();
+        TempData["success"] = "Category deleted successfully";
+
         return RedirectToAction("Index");
 
     
